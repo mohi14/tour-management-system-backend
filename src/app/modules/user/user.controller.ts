c0accab88a1029f9dcc9038b0d3aa6da2adf7904
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-05 23:19:23 +0200

    refactor(api): optimize response status

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 // const createUserFunction = async (req: Response, res: Response) => {
 
@@ -31,13 +32,34 @@ const createUser = catchAsync(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
-    res.status(httpStatus.CREATED).json({
+
+    // res.status(httpStatus.CREATED).json({
+    //   message: "User Created Successfully",
+    //   user,
+    // });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
       message: "User Created Successfully",
-      user,
+      data: user,
     });
   },
 );
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getAllUsers = catchAsync(async(req:Request, res: Response, next: NextFunction)=>{
+  const result = await UserServices.getAllUsers();
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "All Users Retrieved Successfully",
+        data: result.data,
+        meta: result.meta
+    })
+})
+
 export const UserControllers = {
   createUser,
+  getAllUsers
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,10 +1,21 @@
 import { IUser } from "./user.interface";
 import { User } from "./user.model";
 
-const createUser =async(payload: Partial<IUser>)=>{
-    const {name, email}= payload;
-    const user = await User.create({name,email})
-    return user
-}
+const createUser = async (payload: Partial<IUser>) => {
+  const { name, email } = payload;
+  const user = await User.create({ name, email });
+  return user;
+};
 
-export const UserServices = {createUser}
+const getAllUsers = async () => {
+  const users = await User.find({});
+  const totalUsers = await User.countDocuments();
+  return {
+    data: users,
+    meta: {
+      total: totalUsers,
+    },
+  };
+};
+
+export const UserServices = { createUser, getAllUsers };
