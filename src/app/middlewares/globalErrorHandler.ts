f1eba4fadf8ac6560df2929742e8d4023882dfd4
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-05 22:13:41 +0200

    feat(error): custom error class AppError created

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
+import AppError from "../errorHelpers/AppError";
 
 export const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -9,8 +10,16 @@ export const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ) => {
-  const statusCode = 500;
-  const message = "Something Went Wrong!!";
+  let statusCode = 500;
+  let message = "Something Went Wrong!!";
+
+  if (error instanceof AppError) {
+    statusCode = error.statusCode;
+    message = error.message;
+  } else if (error instanceof Error) {
+    statusCode = 500;
+    message = error.message;
+  }
 
   res.status(statusCode).json({
     success: false,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,6 +3,31 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 
+
+
+// const createUserFunction = async (req: Response, res: Response) => {
+
+//     const user = await UserServices.createUser(req.body)
+
+//     res.status(httpStatus.CREATED).json({
+//         message: "User Created Successfully",
+//         user
+//     })
+// }
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         // throw new Error("Fake eror")
+//         // throw new AppError(httpStatus.BAD_REQUEST, "fake error")
+
+//         // createUserFunction(req, res)
+
+//     } catch (err: any) {
+//         console.log(err);
+//         next(err)
+//     }
+// }
+
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = await UserServices.createUser(req.body);
