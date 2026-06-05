e5e9a2934fdd330ae092c817ab1ed2356294b75e
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-05 21:19:37 +0200

    feat(main): organizing routing system

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { router } from "./app/routes";
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-console.log(process.env.DB_URL,"mohii");
-
+app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
