4a20d340cf365052f5f3d4eadcf1a5bf78d2ac55
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-05 22:53:45 +0200

    feat(error): added catchAsync higher order function to avoid Repetition of try-catch

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,8 +2,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
-
-
+import { catchAsync } from "../../utils/catchAsync";
 
 // const createUserFunction = async (req: Response, res: Response) => {
 
@@ -28,19 +27,16 @@ import { UserServices } from "./user.service";
 //     }
 // }
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+const createUser = catchAsync(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
     res.status(httpStatus.CREATED).json({
       message: "User Created Successfully",
       user,
     });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    console.log(error);
-    next(error)
-  }
-};
+  },
+);
 
 export const UserControllers = {
   createUser,
