fcac465f8e2e583a6bd231782ca0b54c426c4e93
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-06 19:19:03 +0200

    feat(api): update user api completed

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,36 +1,41 @@
-import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
-import httpStatus from "http-status-codes";
-import { User } from "../user/user.model";
 import bcrypt from "bcryptjs";
+import httpStatus from "http-status-codes";
+import { envVars } from "../../config/env";
+import AppError from "../../errorHelpers/AppError";
+import { generateToken } from "../../utils/jwt";
+import { IUser } from "../user/user.interface";
+import { User } from "../user/user.model";
+
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
-  const { email, password } = payload;
+    const { email, password } = payload;
 
-  const isUserExist = await User.findOne({ email });
+    const isUserExist = await User.findOne({ email })
 
-  if (!isUserExist) {
-    throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
-  }
-  
+    if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist")
+    }
 
-  const isPasswordMatched = await bcrypt.compare(
-    password as string,
-    isUserExist.password as string,
-  );
+    const isPasswordMatched = await bcrypt.compare(password as string, isUserExist.password as string)
 
-  if (!isPasswordMatched) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password");
-  }
+    if (!isPasswordMatched) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password")
+    }
+    const jwtPayload = {
+        userId: isUserExist._id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES)
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password: pass, ...rest } = isUserExist.toObject()
+    return {
+        accessToken
+    }
 
-  return {
-    ...rest
-  };
-};
+}
+
+//user - login - token (email, role, _id) - booking / payment / booking / payment cancel - token 
 
 export const AuthServices = {
-  credentialsLogin,
-};
+    credentialsLogin
+}
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
 
@@ -9,5 +9,6 @@ const router = Router();
 
 router.post("/register", validateRequest(createUserZodSchema), UserControllers.createUser);
 router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), UserControllers.getAllUsers)
+router.patch("/:id", validateRequest(updateUserZodSchema), checkAuth(...Object.values(Role)), UserControllers.updateUser)
 
 export const UserRoutes = router;
