25eddf8c45ca18050752c5498eba3966d7b4dab8
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-08 01:39:29 +0200

    feat(auth): set the accesstoken and refreshtoken in cookies

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,12 +3,14 @@ import { catchAsync } from "../../utils/catchAsync";
 import { AuthServices } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
+import { setAuthCookie } from "../../utils/setCookie";
 
 const credentialsLogin = catchAsync(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async (req: Request, res: Response, next: NextFunction) => {
     const loginInfo = await AuthServices.credentialsLogin(req.body);
 
+    setAuthCookie(res, loginInfo);
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
@@ -19,21 +21,24 @@ const credentialsLogin = catchAsync(
 );
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const getNewAccessToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.headers.authorization;
-   
-    const tokenInfo = await AuthServices.getNewAccessToken(refreshToken as string)
 
-
+    const tokenInfo = await AuthServices.getNewAccessToken(
+      refreshToken as string,
+    );
+    setAuthCookie(res, tokenInfo);
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: "New Access Token Retrived Successfully",
-        data: tokenInfo,
-    })
-})
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "New Access Token Retrived Successfully",
+      data: tokenInfo,
+    });
+  },
+);
 
 export const AuthControllers = {
   credentialsLogin,
-  getNewAccessToken
+  getNewAccessToken,
 };
