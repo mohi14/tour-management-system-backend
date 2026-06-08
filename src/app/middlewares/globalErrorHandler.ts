a7552cfe5e41d5a556ca49242589cf032f51e048
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-08 20:33:06 +0200

    fix(error): handle mongoose duplicate, cast and validation errors

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import AppError from "../errorHelpers/AppError";
+import { handlerDuplicateError } from "../helpers/handleDuplicateError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handlerValidationError } from "../helpers/handlerValidationError";
+import { TErrorSources } from "../interfaces/error.types";
 
 export const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -10,10 +14,31 @@ export const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ) => {
+  let errorSources: TErrorSources[] = [];
   let statusCode = 500;
   let message = "Something Went Wrong!!";
 
-  if (error instanceof AppError) {
+  //Duplicate error
+  if (error.code === 11000) {
+    const simplifiedError = handlerDuplicateError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  // Object ID error / Cast Error
+  else if (error.name === "CastError") {
+    const simplifiedError = handleCastError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  //Mongoose Validation Error
+  else if (error.name === "ValidationError") {
+    const simplifiedError = handlerValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+    message = simplifiedError.message;
+  } else if (error instanceof AppError) {
     statusCode = error.statusCode;
     message = error.message;
   } else if (error instanceof Error) {
@@ -24,6 +49,7 @@ export const globalErrorHandler = (
   res.status(statusCode).json({
     success: false,
     message,
+    errorSources,
     error: envVars.NODE_ENV === "development" ? error : null,
     stack: envVars.NODE_ENV === "development" ? error.stack : null,
   });
