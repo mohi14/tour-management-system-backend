71874e20ae048565f39e1d8c01fda4eeb533c09c
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-09 14:53:16 +0200

    feat(tour): add tour services

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { DivisionRoutes } from "../modules/division/division.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 
 export const router = Router();
 
@@ -15,9 +16,13 @@ const moduleRoutes = [
     route: AuthRoutes,
   },
   {
-    path:"/division",
-    route: DivisionRoutes
-  }
+    path: "/division",
+    route: DivisionRoutes,
+  },
+  {
+    path: "/tour",
+    route: TourRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => {
