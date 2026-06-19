11a9ea1e824adab1599021a1cc0782482be5af76
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-19 20:50:04 +0200

    refactor(tour): tour model and insert updated fields into database

--- src/app/modules/tour/tour.model.ts
+++ src/app/modules/tour/tour.model.ts
@@ -12,16 +12,17 @@ const tourTypeSchema = new Schema<ITourType>(
 
 export const TourType = model<ITourType>("TourType", tourTypeSchema);
 
-const tourSchema = new Schema<ITour>(
-  {
+const tourSchema = new Schema<ITour>({
     title: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
+    slug: { type: String, unique: true },
     description: { type: String },
     images: { type: [String], default: [] },
     location: { type: String },
     costFrom: { type: Number },
     startDate: { type: Date },
     endDate: { type: Date },
+    departureLocation: { type: String },
+    arrivalLocation: { type: String },
     included: { type: [String], default: [] },
     excluded: { type: [String], default: [] },
     amenities: { type: [String], default: [] },
@@ -29,20 +30,19 @@ const tourSchema = new Schema<ITour>(
     maxGuest: { type: Number },
     minAge: { type: Number },
     division: {
-      type: Schema.Types.ObjectId,
-      ref: "Division",
-      required: true,
+        type: Schema.Types.ObjectId,
+        ref: "Division",
+        required: true
     },
     tourType: {
-      type: Schema.Types.ObjectId,
-      ref: "TourType",
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  },
-);
+        type: Schema.Types.ObjectId,
+        ref: "TourType",
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
 
 tourSchema.pre("save", async function () {
   if (this.isModified("title")) {
