3ee5c2dda2d65e677d57cdc70434873c1982156a
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-21 14:49:17 +0200

    feat(division): upload single image for division

--- src/app/middlewares/validateRequest.ts
+++ src/app/middlewares/validateRequest.ts
@@ -4,6 +4,9 @@ import { ZodType } from "zod"
 
 export const validateRequest = <T extends ZodType>(zodSchema: T) => async (req: Request, res: Response, next: NextFunction) => {
     try {
+        if(req.body.data){
+            req.body = JSON.parse(req.body.data)
+        }
         req.body = await zodSchema.parseAsync(req.body)
         next()
     } catch (error) {
