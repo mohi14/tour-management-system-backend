b698428cd9d67a07752b364eb2e45b4d23837de5
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-23 00:34:07 +0200

    refactor(payment): sslcommerce validate payment route created

--- src/app/modules/sslCommerz/sslCommerz.service.ts
+++ src/app/modules/sslCommerz/sslCommerz.service.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
 import axios from "axios"
 import httpStatus from "http-status-codes"
 import { envVars } from "../../config/env"
 import AppError from "../../errorHelpers/AppError"
 import { ISSLCommerz } from "./sslCommerz.interface"
+import { Payment } from "../payment/payment.model"
 
 const sslPaymentInit = async (payload: ISSLCommerz) => {
 
@@ -56,6 +59,26 @@ const sslPaymentInit = async (payload: ISSLCommerz) => {
     }
 }
 
+const validatePayment = async (payload: any) => {
+    try {
+        const response = await axios({
+            method: "GET",
+            url: `${envVars.SSL.SSL_VALIDATION_API}?val_id=${payload.val_id}&store_id=${envVars.SSL.STORE_ID}&store_passwd=${envVars.SSL.STORE_PASS}`
+        })
+
+        console.log("sslcomeerz validate api response", response.data);
+
+        await Payment.updateOne(
+            { transactionId: payload.tran_id },
+            { paymentGatewayData: response.data },
+            { runValidators: true })
+    } catch (error: any) {
+        console.log(error);
+        throw new AppError(401, `Payment Validation Error, ${error.message}`)
+    }
+}
+
 export const SSLService = {
-    sslPaymentInit
+    sslPaymentInit,
+    validatePayment
 }
