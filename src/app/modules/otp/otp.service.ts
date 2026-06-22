0da5ec5be84f569306a4a5723955d663334e9775
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-22 22:30:32 +0200

    feat(otp): verify otp api implemented

--- src/app/modules/otp/otp.service.ts
+++ src/app/modules/otp/otp.service.ts
@@ -39,6 +39,39 @@ const sendOTP = async (email: string, name: string) => {
   });
 };
 
+
+const verifyOTP = async (email: string, otp: string) => {
+    // const user = await User.findOne({ email, isVerified: false })
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new AppError(404, "User not found")
+    }
+
+    if (user.isVerified) {
+        throw new AppError(401, "You are already verified")
+    }
+
+    const redisKey = `otp:${email}`
+
+    const savedOtp = await redisClient.get(redisKey)
+
+    if (!savedOtp) {
+        throw new AppError(401, "Invalid OTP");
+    }
+
+    if (savedOtp !== otp) {
+        throw new AppError(401, "Invalid OTP");
+    }
+
+
+    await Promise.all([
+        User.updateOne({ email }, { isVerified: true }, { runValidators: true }),
+        redisClient.del([redisKey])
+    ])
+
+};
 export const OTPService = {
   sendOTP,
+  verifyOTP
 };
