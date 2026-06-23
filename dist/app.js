0b62b95633687c5f92fa59d22124a9a2bd018a04
mohi14 <mohiuddinkhan1409@gmail.com>
2026-06-23 02:06:36 +0200

    fix(build): vercel deployment error fixed

--- dist/app.js
+++ dist/app.js
@@ -23,12 +23,18 @@ app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
+app.set("trust proxy", 1);
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)({
     origin: env_1.envVars.FRONTEND_URL,
     credentials: true
 }));
 app.use("/api/v1", routes_1.router);
+app.use("/helth", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to Tour Management System Backend",
+    });
+});
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "Welcome to Tour Management System Backend",
--- dist/app/utils/setCookie.js
+++ dist/app/utils/setCookie.js
@@ -1,17 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setAuthCookie = void 0;
+const env_1 = require("../config/env");
+// export const setAuthCookie = (res:Response, tokenInfo:AuthTokens )=>{
+//      if (tokenInfo.accessToken) {
+//         res.cookie("accessToken", tokenInfo.accessToken, {
+//             httpOnly: true,
+//             secure: false
+//         })
+//     }
+//     if (tokenInfo.refreshToken) {
+//         res.cookie("refreshToken", tokenInfo.refreshToken, {
+//             httpOnly: true,
+//             secure: false,
+//         })
+//     }
+// }
 const setAuthCookie = (res, tokenInfo) => {
     if (tokenInfo.accessToken) {
         res.cookie("accessToken", tokenInfo.accessToken, {
             httpOnly: true,
-            secure: false
+            secure: env_1.envVars.NODE_ENV === "production",
+            sameSite: "none"
         });
     }
     if (tokenInfo.refreshToken) {
         res.cookie("refreshToken", tokenInfo.refreshToken, {
             httpOnly: true,
-            secure: false,
+            secure: env_1.envVars.NODE_ENV === "production",
+            sameSite: "none"
         });
     }
 };
